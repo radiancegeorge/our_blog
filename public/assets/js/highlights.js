f12929255a43f3a8af828f458edfecf607bd6900
radiancegeorge <radiancegeorge@outlook.com>
2020-04-15 15:18:12 +0100

    more animations and sliders comming up next

---
+++ public/assets/js/highlights.js
@@ -0,0 +1,38 @@
+const cols = document.querySelectorAll('.highlights .col');
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+
+let time = 0.5
+cols.forEach(col => {
+    const colChildren = col.children; 
+
+    colChildren.forEach( child =>{
+        gsap.from(child, {
+        y: 400,
+        duration: time,
+        opacity: 0
+    })
+    })
+
+    colChildren.forEach(child =>{
+        child.addEventListener('mouseenter',(e)=>{
+                console.log('over');
+                gsap.to(e.target,{
+                    y: -5,
+                    rotateZ: 2
+                })
+            })
+    })
+    
+    colChildren.forEach( child =>{
+        child.addEventListener('mouseleave',(e)=>{
+        console.log('over');
+        gsap.to(e.target,{
+            y: 5,
+            rotateZ: 0
+        })
+    })
+    })
+    
+    return time++
+});
+
